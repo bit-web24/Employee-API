7fa4b91e9b99c86c4d7d01a6d8bf486be9fe55a3
bit-web24 <bitweb24@gmail.com>
2023-05-31 19:59:56 +0530

    Basic App Structure

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./src/routes');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/', routes);
+
+app.listen(PORT, ()=>{
+    console.log(`API server running on port: ${PORT}`);
+});
