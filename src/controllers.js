66fc54896c262e14740e7c90a82d9d04ce4cb3bb
bit-web24 <bitweb24@gmail.com>
2023-05-31 22:03:55 +0530

    impl updateEmployee

--- src/controllers.js
+++ src/controllers.js
@@ -1,48 +1,58 @@
 const { Employee } = require('./models');
 
 const createEmployee = (req, res) => {
-  const { name, email, phone } = req.body;
+    const { name, email, phone } = req.body;
 
-  Employee.create({ name, email, phone })
-    .then(() => {
-      res.status(201).json({ message: 'Employee created successfully' });
-    })
-    .catch((error) => {
-      console.error('Error creating employee:', error);
-      res.status(500).json({ error: 'Failed to create employee' });
-    });
+    Employee.create({ name, email, phone })
+        .then(() => {
+            res.status(201).json({ message: 'Employee created successfully' });
+        })
+        .catch((error) => {
+            console.error('Error creating employee:', error);
+            res.status(500).json({ error: 'Failed to create employee' });
+        });
 };
 
 const listEmployees = (req, res) => {
-  Employee.findAll()
-    .then((employees) => {
-      res.json({ employees });
-    })
-    .catch((error) => {
-      console.error('Error retrieving employees:', error);
-      res.status(500).json({ error: 'Failed to retrieve employees' });
-    });
+    Employee.findAll()
+        .then((employees) => {
+            res.json({ employees });
+        })
+        .catch((error) => {
+            console.error('Error retrieving employees:', error);
+            res.status(500).json({ error: 'Failed to retrieve employees' });
+        });
 };
 
 const getEmployee = (req, res) => {
     const employeeId = req.params.id;
-  
+
     Employee.findByPk(employeeId)
-      .then((employee) => {
-        if (employee) {
-          res.json({ employee });
-        } else {
-          res.status(404).json({ error: 'Employee not found' });
-        }
-      })
-      .catch((error) => {
-        console.error('Error retrieving employee:', error);
-        res.status(500).json({ error: 'Failed to retrieve employee' });
-      });
-  };
+        .then((employee) => {
+            if (employee) {
+                res.json({ employee });
+            } else {
+                res.status(404).json({ error: 'Employee not found' });
+            }
+        })
+        .catch((error) => {
+            console.error('Error retrieving employee:', error);
+            res.status(500).json({ error: 'Failed to retrieve employee' });
+        });
+};
 
 const updateEmployee = (req, res) => {
+    const { name, email, phone } = req.body;
+    const employeeId = req.params.id;
 
+    Employee.update({ name, email, phone }, { where: { id: employeeId } })
+        .then(() => {
+            res.status(200).json({ message: 'Employee updated successfully' });
+        })
+        .catch((error) => {
+            console.log('Error updating Employee:', error);
+            res.status(500).json({ error: 'Failed to update Employee' });
+        });
 }
 
 const deleteEmployee = (req, res) => {
