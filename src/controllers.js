380290bcbd8433666247a11d91294720d3d2ea74
bit-web24 <bitweb24@gmail.com>
2023-05-31 20:21:01 +0530

    defined controllers

---
+++ src/controllers.js
@@ -0,0 +1,27 @@
+function createEmployee(){
+
+}
+
+function listEmployees(){
+
+}
+
+function getEmployee(){
+
+}
+
+function updateEmployee(){
+
+}
+
+function deleteEmployee(){
+
+}
+
+module.exports = {
+    createEmployee,
+    listEmployees,
+    getEmployee,
+    updateEmployee,
+    deleteEmployee
+};
