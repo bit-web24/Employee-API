8727cd406a9710a378270d83488d2a6f7c7ccfe1
bit-web24 <bitweb24@gmail.com>
2023-05-31 21:48:54 +0530

    update createEmployee and listEmployees controllers

--- src/controllers.js
+++ src/controllers.js
@@ -1,10 +1,28 @@
-const createEmployee = (req, res) => {
+const { Employee } = require('./models');
 
-}
+const createEmployee = (req, res) => {
+  const { name, email, phone } = req.body;
+
+  Employee.create({ name, email, phone })
+    .then(() => {
+      res.status(201).json({ message: 'Employee created successfully' });
+    })
+    .catch((error) => {
+      console.error('Error creating employee:', error);
+      res.status(500).json({ error: 'Failed to create employee' });
+    });
+};
 
 const listEmployees = (req, res) => {
-
-}
+  Employee.findAll()
+    .then((employees) => {
+      res.json({ employees });
+    })
+    .catch((error) => {
+      console.error('Error retrieving employees:', error);
+      res.status(500).json({ error: 'Failed to retrieve employees' });
+    });
+};
 
 const getEmployee = (req, res) => {
 
