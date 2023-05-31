09c876d35aa1d85aff6278ef935b9c90f5b09133
bit-web24 <bitweb24@gmail.com>
2023-06-01 01:15:17 +0530

    added express-paginate & limit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./src/routes');
+const paginate = require("express-paginate");
 
 require('dotenv').config();
 
@@ -9,6 +10,10 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(bodyParser.json());
 app.use('/', routes);
+app.use(express.urlencoded({
+	extended: false
+}));
+app.use(paginate.middleware(5, 10));
 
 app.listen(PORT, ()=>{
     console.log(`API server running on port: ${PORT}`);
