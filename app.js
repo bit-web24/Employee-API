4b795b5bd04ab4e40cc47e8de825fe02a70ec317
bit-web24 <bitweb24@gmail.com>
2023-06-01 21:45:51 +0530

    optimization

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const { json } = require('body-parser');
 const routes = require('./src/routes');
-const paginate = require("express-paginate");
+const paginate = require('express-paginate');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(bodyParser.json());
-app.use('/', routes);
-app.use(express.urlencoded({
-	extended: false
-}));
-app.use(paginate.middleware(5, 10));
 
-app.listen(PORT, ()=>{
-    console.log(`API server running on port: ${PORT}`);
+app.use(json());
+app.use(paginate.middleware(5, 10));
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`API server running on port: ${PORT}`);
 });
--- src/controllers.js
+++ src/controllers.js
@@ -1,38 +1,34 @@
 const { Employee } = require('./models');
 
-const createEmployee = (req, res) => {
-    const { name, email, phone } = req.body;
-
-    Employee.create({ name, email, phone })
-        .then(() => {
-            return res.status(201).json({ message: 'Employee created successfully' });
-        })
-        .catch((error) => {
-            console.error('Error creating employee:', error);
-            return res.status(500).json({ error: 'Failed to create employee' });
-        });
+const createEmployee = async (req, res) => {
+    try {
+        const { name, email, phone } = req.body;
+        await Employee.create({ name, email, phone });
+        return res.status(201).json({ message: 'Employee created successfully' });
+    } catch (error) {
+        return res.status(500).json({ error: 'Failed to create employee' });
+    }
 };
 
-const listEmployees = (req, res) => {
-    const page = req.query.page ? parseInt(req.query.page) : 1;
-    const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+const listEmployees = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const offset = (page - 1) * limit;
 
-    const offset = (page - 1) * limit;
-    Employee.findAll({
-        limit: limit,
-        offset: offset
-    })
-        .then((employees) => {
-            return res.json({
-                page: page,
-                limit: limit,
-                data: employees
-            });
-        })
-        .catch((error) => {
-            console.error('Error retrieving employees:', error);
-            return res.status(500).json({ error: 'Failed to retrieve employees' });
+        const employees = await Employee.findAll({
+            limit: limit,
+            offset: offset
         });
+
+        return res.json({
+            page,
+            limit,
+            data: employees
+        });
+    } catch (error) {
+        return res.status(500).json({ error: 'Failed to retrieve employees' });
+    }
 };
 
 const getEmployee = async (req, res) => {
@@ -43,42 +39,44 @@ const getEmployee = async (req, res) => {
             return res.status(404).json({ error: 'Employee not found' });
         }
         return res.json({ foundEmployee });
-    }
-    catch (error) {
+    } catch (error) {
         console.error('Error retrieving employee:', error);
         return res.status(500).json({ error: 'Failed to retrieve employee' });
-    };
+    }
 };
 
 const updateEmployee = async (req, res) => {
     const { name, email, phone } = req.body;
     const employeeId = req.params.id;
     try {
-        const updated = await Employee.update({ name, email, phone }, { where: { id: employeeId } });
-        if (updated == 0) {
+        const [rowsUpdated] = await Employee.update(
+            { name, email, phone },
+            { where: { id: employeeId } }
+        );
+        if (rowsUpdated === 0) {
             return res.status(404).json({ error: 'Employee not found' });
         }
         return res.status(200).json({ message: 'Employee updated successfully' });
-    }
-    catch (error) {
-        console.log('Error updating Employee:', error);
+    } catch (error) {
+        console.error('Error updating Employee:', error);
         return res.status(500).json({ error: 'Failed to update Employee' });
-    };
-}
+    }
+};
 
 const deleteEmployee = async (req, res) => {
     const employeeId = req.params.id;
     try {
-        const destroyed = await Employee.destroy({ where: { id: employeeId } });
-        if (!destroyed) {
+        const rowsDeleted = await Employee.destroy({ where: { id: employeeId } });
+        if (rowsDeleted === 0) {
             return res.status(404).json({ error: 'Employee not found' });
         }
         return res.status(200).json({ message: 'Employee deleted successfully' });
-    }
-    catch (error) {
+    } catch (error) {
+        console.error('Error deleting Employee', error);
         return res.status(500).json({ error: 'Failed to delete employee' });
-    };
-}
+    }
+};
+
 
 module.exports = {
     createEmployee,
